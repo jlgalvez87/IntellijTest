ad9457178c80771feb691f4502abffc6873bf8a8
josevazquez101987 <josevazquez101987@gmail.com>
2018-03-30 15:22:53 -0400

    adding properties

--- src/com/cu/PruebaGit.java
+++ src/com/cu/PruebaGit.java
@@ -3,4 +3,22 @@ package com.cu;
 public class PruebaGit {
 
     private String pp = "hola";
+
+    private OtraClase oneToOne;
+
+    public String getPp() {
+        return pp;
+    }
+
+    public void setPp(String pp) {
+        this.pp = pp;
+    }
+
+    public OtraClase getOneToOne() {
+        return oneToOne;
+    }
+
+    public void setOneToOne(OtraClase oneToOne) {
+        this.oneToOne = oneToOne;
+    }
 }
